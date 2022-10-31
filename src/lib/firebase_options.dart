4a77315bc038382979b377826eac3b2e52a731e0
Alien_job <Alien_job@list.ru>
2022-10-31 05:43:03 +0300

    firebase options

---
+++ src/lib/firebase_options.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyAlsRRFeugq5VKsUdBJ8pXOK17lBdRsyMQ',
+    appId: '1:167613826608:android:3a41ded2bb3a7be19d4ff5',
+    messagingSenderId: '167613826608',
+    projectId: 'pashutin-7022b',
+    storageBucket: 'pashutin-7022b.appspot.com',
+  );
+}
